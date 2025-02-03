466029685dc2f7716bf583c7635e66f905c086d4
PanDyyG <angel.camru@gmail.com>
2025-02-03 20:53:41 +0100

    validacion de login hecha

--- register.php
+++ register.php
@@ -47,7 +47,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container">
-            <a class="navbar-brand" href="#"><i class="fas fa-video"></i> Netflix</a>
+            <a class="navbar-brand" href="index.php"><i class="fas fa-video"></i> Netflix</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
